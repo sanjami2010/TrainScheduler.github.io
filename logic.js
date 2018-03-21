d62f7b8b03d6a7588181fde8f5717741b0af9945
sanj2010 <srisama9@gmail.com>
2018-03-21 05:07:58 -0500

    c1

---
+++ logic.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyDIydOjicYCc0cgKzGtgoNzJ2IDS0w5Plk",
+    authDomain: "myfirstproject-c2e67.firebaseapp.com",
+    databaseURL: "https://myfirstproject-c2e67.firebaseio.com",
+    projectId: "myfirstproject-c2e67",
+    storageBucket: "myfirstproject-c2e67.appspot.com",
+    messagingSenderId: "166402848293"
+};
+firebase.initializeApp(config);
